428a53dc85c9ee1fc9e3574271e9850dab2e2b0d
Stefan.Kummerlöw <gummiz@gmx.net>
2024-02-07 17:34:28 +0100

    detail dialog overlay added

--- src/components/DatailDialog.jsx
+++ src/components/DatailDialog.jsx
@@ -1,13 +1,17 @@
 import "../style/detaildialog.css";
 
-function DetailDialog({ isOpen, onClose, data}) {
+function DetailDialog({ isOpen, onClose, data }) {
   if (!isOpen) return null;
 
   return (
-    <>
-      <h1>Dialog {data.title} </h1>
-      <button onClick={onClose}>Closing</button>
-    </>
+    
+      <div className="detaildialog">
+        <div className="detaildialog__content">
+            <h1>Dialog {data.title} </h1>
+            <button onClick={onClose}>Closing</button>
+        </div>
+      </div>
+  
   );
 }
 
